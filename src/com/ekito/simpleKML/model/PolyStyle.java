24b86df5ebc01428bf39963620f316b10caf8b98
Maxime Britto <maxime.britto@gmail.com>
2015-08-05 15:38:57 +0200

    Reference compliance
    - polyline styles for fill and outline shoud have a default value of
    true when not specified
    - StyleMap pairs are inlined

--- src/com/ekito/simpleKML/model/PolyStyle.java
+++ src/com/ekito/simpleKML/model/PolyStyle.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2012 Ekito - http://www.ekito.fr/
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -36,6 +36,9 @@ public class PolyStyle extends ColorStyle {
 	 * @return the fill
 	 */
 	public Integer getFill() {
+		if (fill == null){
+			return 1;
+		}
 		return fill;
 	}
 
@@ -54,6 +57,9 @@ public class PolyStyle extends ColorStyle {
 	 * @return the outline
 	 */
 	public Integer getOutline() {
+		if (outline == null){
+			return 1;
+		}
 		return outline;
 	}
 
